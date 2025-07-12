96dbc0200a5316985ec5feb26833fa65a0b66e7e
Vamshi Krishna <vamshikrishnadulam2504@gmail.com>
2025-07-12 19:26:27 +0530

    deploying to GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace '<repository-name>' with the actual name of your GitHub repository.
+// For example, if your repo is 'my-react-app', then base: '/my-react-app/'
 export default defineConfig({
   plugins: [react()],
+  base: '/<repository-name>/',
 })
